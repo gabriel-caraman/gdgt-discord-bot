b66e94e3cd03894dd3ad5ebcb4bfa638968160a2
Gabriel Caraman <144449540+gabriel-caraman@users.noreply.github.com>
2026-08-06 14:28:18 +0300

    Node initialization
    Added base modules (discord.js & dotenv) for first connection

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
